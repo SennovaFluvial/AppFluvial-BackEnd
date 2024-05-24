5f23cb77fdfc8414537cc52996af312d60413ba8
santiiago25 <sennova@MacBook-Pro-de-SENNOVA.local>
2024-05-24 15:09:30 -0500

    Primer Commit

---
+++ src/main/java/com/AppFluvial/Envios/SistemaLogisticoDeEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.AppFluvial.Envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLogisticoDeEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLogisticoDeEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AppFluvial/Envios/SistemaLogisticoDeEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppFluvial.Envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLogisticoDeEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
